5306a413139734d9d5dae76f438a8201a7668392
Patrick Labadie <plabadie@users.noreply.github.com>
2014-04-28 01:03:54 -0400

    Initial commit

---
+++ src/main/java/com/plabadie/mockmail/handler/FileOutMessageHandler.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2014 Patrick Labadie
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.plabadie.mockmail.handler;
+
+import org.apache.commons.io.FileUtils;
+import org.subethamail.smtp.MessageContext;
+import org.subethamail.smtp.MessageHandler;
+import org.subethamail.smtp.MessageHandlerFactory;
+import org.subethamail.smtp.RejectException;
+
+import java.io.*;
+
+public class FileOutMessageHandler implements MessageHandlerFactory {
+
+	private int sequence = 0;
+	File fileOutFolder = null;
+	
+    public MessageHandler create(MessageContext ctx) {
+        return new Handler(ctx);
+    }
+    
+
+    class Handler implements MessageHandler {
+        MessageContext ctx;
+
+        public Handler(MessageContext ctx) {
+                this.ctx = ctx;
+        }
+        
+        public void from(String from) throws RejectException {
+        }
+
+        public void recipient(String recipient) throws RejectException {
+        }
+
+        public void data(InputStream data) throws IOException {
+        		String mailData = this.convertStreamToString(data);
+
+        		sequence++;
+                
+                File dataFile = new File( getFileOutFolder().getAbsolutePath() + "/mail-" + sequence + ".eml" );
+                
+                while ( dataFile.exists() ) {
+                	sequence++;
+                	dataFile = new File( getFileOutFolder().getAbsolutePath() + "/mail-" + sequence + ".eml" );
+                }
+                
+                FileUtils.writeStringToFile( dataFile , mailData);
+                System.out.println("Recieved " + dataFile.getName() );
+
+        }
+
+        public void done() {
+        }
+
+        public String convertStreamToString(InputStream is) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                StringBuilder sb = new StringBuilder();
+                
+                String line = null;
+                try {
+                        while ((line = reader.readLine()) != null) {
+                                sb.append(line + "\n");
+                        }
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+                return sb.toString();
+        }
+    }
+
+
+	public File getFileOutFolder() {
+		return fileOutFolder;
+	}
+
+
+	public void setFileOutFolder(File fileOutFolder) {
+		this.fileOutFolder = fileOutFolder;
+	}
+}
---
+++ src/main/java/com/plabadie/mockmail/server/MockMailServer.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright 2014 Patrick Labadie
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.plabadie.mockmail.server;
+
+
+import java.io.File;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.lang3.StringUtils;
+import com.plabadie.mockmail.handler.FileOutMessageHandler;
+import org.subethamail.smtp.server.SMTPServer;
+
+public class MockMailServer {
+
+	private int port;
+	private String outDirPath;
+	
+	public MockMailServer() {
+		super();
+		
+		this.setOutDirPath("mail");
+		this.setPort(2525);
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+
+		MockMailServer mms = new MockMailServer();
+
+		mms.initWithCommandLine( args );
+		
+		mms.serve();
+
+	}
+
+	private void serve() {
+		
+		File outdir = new File(getOutDirPath());
+
+        System.out.println( "MockMail 1.0 SMTP Server starting on port :" + this.getPort()  );
+        System.out.println( "Email output: " + outdir.getAbsolutePath() );
+		
+		FileOutMessageHandler messageHandler = new FileOutMessageHandler();
+		messageHandler.setFileOutFolder( outdir );
+		
+		SMTPServer smtpServer = new SMTPServer(messageHandler);
+		smtpServer.setPort(this.getPort());
+		smtpServer.start();
+	}
+
+	private void initWithCommandLine( String[] args ){
+		
+		Option help = new Option( "help", "print this message" );
+
+		Option port   = OptionBuilder.withArgName( "portnumber" )
+                .hasArg()
+                .withDescription(  "Port number where MockMail serves smtp requests. Default : 2525." )
+                .create( "port" );
+		
+		Option outdir = OptionBuilder.withArgName( "path" )
+                .hasArg()
+                .withDescription(  "Folder where the email files will be stored. Can be relative to the working dir. Default : 'mail'" )
+                .create( "outdir" );
+
+		Options options = new Options();
+
+		options.addOption( help );
+		options.addOption( port );
+		options.addOption( outdir );
+
+		// create the parser
+	    CommandLineParser parser = new GnuParser();
+	    CommandLine line = null;
+	    try {
+	        // parse the command line arguments
+	        line = parser.parse( options, args );
+	    }
+	    catch( ParseException exp ) {
+	        // oops, something went wrong
+	        System.err.println( exp.getMessage() );
+	        System.exit(1);
+	    }		
+
+	    if( line.hasOption( "help" ) ) {
+		    // automatically generate the help statement
+		    HelpFormatter formatter = new HelpFormatter();
+		    formatter.printHelp( "java -jar MockMail.jar", options );
+		    System.exit(0);
+	    }
+
+	    
+	    if( line.hasOption( "port" ) ) {
+	    	String portStringValue = line.getOptionValue( "port" );
+
+	    	if ( StringUtils.isNotBlank( portStringValue ) && StringUtils.isNumeric( portStringValue ) ) {
+	    		
+	    		this.setPort( Integer.parseInt( portStringValue ) );
+	    		
+	    	}
+	    	
+	    }
+	    if( line.hasOption( "outdir" ) ) {
+	    	
+	    	String outDirStringValue = line.getOptionValue( "outdir" ); 
+
+	    	if ( StringUtils.isNotBlank( outDirStringValue  ) ) {
+	    		
+	    		this.setOutDirPath(outDirStringValue);
+	    		
+	    	}
+	    	
+	    }
+	    
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+
+
+	public String getOutDirPath() {
+		return outDirPath;
+	}
+
+
+
+	public void setOutDirPath(String outDirPath) {
+		this.outDirPath = outDirPath;
+	}
+	
+}
