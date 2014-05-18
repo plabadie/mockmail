f82dfb75d9a48a18b4950c70f2e1f035d573f62b
Patrick Labadie <plabadie@users.noreply.github.com>
2014-05-18 12:15:28 -0400

    Implemented a properties file based application metadata. 	modified:   pom.xml 	modified:   src/main/java/com/plabadie/mockmail/util/App.java 	new file:   src/main/resources/application.properties

--- src/main/java/com/plabadie/mockmail/util/App.java
+++ src/main/java/com/plabadie/mockmail/util/App.java
@@ -1,15 +1,59 @@
 package com.plabadie.mockmail.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Created by plabadie on 2014-05-18.
  */
 public class App
 {
-    private static String version = "1.1";
-
+    private static Logger logger = LoggerFactory.getLogger( App.class );
+    private static Properties properties;
 
     public static String getVersion()
     {
-        return version;
+        return getProperties().getProperty( "version" );
+    }
+
+    public static Properties getProperties()
+    {
+        if ( properties != null )
+        {
+            return properties;
+        }
+
+        properties = new Properties();
+        InputStream input = null;
+
+        try
+        {
+            input = App.class.getClassLoader().getResourceAsStream( "application.properties" );
+            properties.load( input );
+        }
+        catch ( Exception e )
+        {
+            logger.error( "An error occured trying to read the application properties" , e );
+        }
+        finally
+        {
+            if ( input != null )
+            {
+                try
+                {
+                    input.close();
+                }
+                catch ( IOException e )
+                {
+                    logger.error( "An error occured trying to close the application properties"  );
+                }
+            }
+        }
+
+        return properties;
     }
 }
