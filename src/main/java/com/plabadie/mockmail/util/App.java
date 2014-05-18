14714e1683ae2533f24dd25279ed6d0876d523a2
Patrick Labadie <plabadie@users.noreply.github.com>
2014-05-18 12:17:29 -0400

    Minor changes to comments. 	modified:   src/main/java/com/plabadie/mockmail/util/App.java

--- src/main/java/com/plabadie/mockmail/util/App.java
+++ src/main/java/com/plabadie/mockmail/util/App.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 Patrick Labadie
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.plabadie.mockmail.util;
 
 import org.slf4j.Logger;
@@ -7,9 +22,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-/**
- * Created by plabadie on 2014-05-18.
- */
 public class App
 {
     private static Logger logger = LoggerFactory.getLogger( App.class );
